46829b2acfca320c0e8026e1fba89882c620f042
Justin O'Connor <jsoconno@gmail.com>
2023-08-07 17:07:15 -0400

    try adding additional next.config.js settings

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: "standalone",
+    experimental: {
+      serverActions: true,
+      serverActionsBodySizeLimit: "22mb",
+    },
+  };
+  
+  module.exports = nextConfig;
