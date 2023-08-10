571b0049f1000194252dabca263dc68b843c248c
Justin O'Connor <jsoconno@gmail.com>
2023-08-10 08:43:20 -0400

    test using msi

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   # storage_use_azuread = true
+  use_msi = true
 
   features {
     resource_group {
@@ -17,8 +18,8 @@ provider "azurerm" {
     }
   }
 
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,25 +1,25 @@
 # Project variables
 
-variable "tenant_id" {
-  description = "The Azure Tenant ID"
-  type        = string
-}
+# variable "tenant_id" {
+#   description = "The Azure Tenant ID"
+#   type        = string
+# }
 
-variable "subscription_id" {
-  description = "The Azure Subscription ID"
-  type        = string
-}
+# variable "subscription_id" {
+#   description = "The Azure Subscription ID"
+#   type        = string
+# }
 
-variable "client_id" {
-  description = "The Azure Client ID"
-  type        = string
-}
+# variable "client_id" {
+#   description = "The Azure Client ID"
+#   type        = string
+# }
 
-variable "client_secret" {
-  description = "The Azure Client Secret"
-  type        = string
-  sensitive   = true
-}
+# variable "client_secret" {
+#   description = "The Azure Client Secret"
+#   type        = string
+#   sensitive   = true
+# }
 
 variable "name" {
   type        = string
