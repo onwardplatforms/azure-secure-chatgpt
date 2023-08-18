78af5a8dad6893274427d2be5dcd97aec5608f1d
Justin O'Connor <jsoconno@gmail.com>
2023-08-18 09:31:40 -0400

    add cognitivie service account details to app settings for model queries, add hidden tags for app insights, and add rbac for storage blob and cognitive account

--- infra/r-function-app.tf
+++ infra/r-function-app.tf
@@ -26,8 +26,19 @@ resource "azurerm_linux_function_app" "main" {
     COSMOSDB_URL      = azurerm_cosmosdb_account.main.endpoint
     COSMOSDB_KEY      = azurerm_cosmosdb_account.main.primary_key
     COSMOSDB_DATABASE = azurerm_cosmosdb_sql_database.main.name
+    SUBSCRIPTION_ID                                 = data.azurerm_client_config.current.subscription_id
+    RESOURCE_GROUP_NAME                             = azurerm_resource_group.application.name
+    COGNITIVE_ACCOUNT_NAME                          = azurerm_cognitive_account.main.name
     WEBSITE_RUN_FROM_PACKAGE = azurerm_storage_blob.function_app_code.url
   }
+
+  tags = merge(
+    var.tags,
+    {
+      "hidden-link: /app-insights-instrumentaiton-key" = azurerm_application_insights.main.instrumentation_key
+      "hidden-link: /app-insights-connection-string"  = azurerm_application_insights.main.connection_string
+    }
+  )
 }
 
 resource "azurerm_role_assignment" "cosmos_contributor" {
@@ -36,7 +47,19 @@ resource "azurerm_role_assignment" "cosmos_contributor" {
     principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
 }
 
-data "archive_file" "function_app_code_zip" {
+resource "azurerm_role_assignment" "storage_blob_data_contibutor" {
+    scope                = azurerm_storage_account.main.id
+    role_definition_name = "DocumentDB Account Contributor"
+    principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "cognitive_services_contributor" {
+    scope                = azurerm_cognitive_account.main.id
+    role_definition_name = "Contributor"
+    principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+}
+
+data "archive_file" "function_app_code" {
   type        = "zip"
   source_dir  = "${path.module}/../functions"
   output_path = "${path.module}/function-code.zip"
@@ -47,5 +70,6 @@ resource "azurerm_storage_blob" "function_app_code" {
   storage_account_name   = azurerm_storage_account.main.name
   storage_container_name = azurerm_storage_container.function_releases.name
   type                   = "Block"
-  source                 = data.archive_file.function_app_code_zip.output_path
+  source                 = data.archive_file.function_app_code.output_path
+  content_md5            = data.archive_file.function_app_code.output_md5
 }
