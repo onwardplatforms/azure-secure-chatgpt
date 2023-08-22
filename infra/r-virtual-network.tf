e574876ef3c7b5f3a6cd49a12bc840d6dd5f2573
Justin O'Connor <jsoconno@gmail.com>
2023-08-22 11:40:36 -0400

    make the creation of the virtual network resource group optional depending on if teh configuration is public or private

--- infra/r-virtual-network.tf
+++ infra/r-virtual-network.tf
@@ -2,16 +2,16 @@ resource "azurerm_network_security_group" "main" {
   count = var.public_network_access_enabled ? 0 : 1
 
   name                = "nsg-${local.project_name}"
-  location            = azurerm_resource_group.networking.location
-  resource_group_name = azurerm_resource_group.networking.name
+  location            = azurerm_resource_group.networking[0].location
+  resource_group_name = azurerm_resource_group.networking[0].name
 }
 
 resource "azurerm_virtual_network" "main" {
   count = var.public_network_access_enabled ? 0 : 1
 
   name                = "vnet-${local.project_name}"
-  location            = azurerm_resource_group.networking.location
-  resource_group_name = azurerm_resource_group.networking.name
+  location            = azurerm_resource_group.networking[0].location
+  resource_group_name = azurerm_resource_group.networking[0].name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "private_endpoints" {
   count = var.public_network_access_enabled ? 0 : 1
 
   name                 = "snet-${local.project_name}-private-endpoints"
-  resource_group_name  = azurerm_resource_group.networking.name
+  resource_group_name  = azurerm_resource_group.networking[0].name
   virtual_network_name = azurerm_virtual_network.main[0].name
   address_prefixes     = ["10.0.1.0/24"]
 
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "app_services" {
   count = var.public_network_access_enabled ? 0 : 1
 
   name                 = "snet-${local.project_name}-app-services"
-  resource_group_name  = azurerm_resource_group.networking.name
+  resource_group_name  = azurerm_resource_group.networking[0].name
   virtual_network_name = azurerm_virtual_network.main[0].name
   address_prefixes     = ["10.0.2.0/24"]
 
