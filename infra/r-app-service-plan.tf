650eeba180600382469d06aa7ae67b7208917fb3
Justin O'Connor <jsoconno@gmail.com>
2023-08-22 11:35:54 -0400

    revert to using the variable for app_service_plan_sku to set the sku as part of an overall serverless logic backout

--- infra/r-app-service-plan.tf
+++ infra/r-app-service-plan.tf
@@ -6,19 +6,12 @@
  *
  */
 
-locals {
-  is_serverless_and_public  = var.enable_serverless && var.public_network_access_enabled
-  is_serverless_and_private = var.enable_serverless && !var.public_network_access_enabled
-
-  sku_name = local.is_serverless_and_public ? "Y1" : local.is_serverless_and_private ? "EP1" : var.app_service_plan_sku
-}
-
 resource "azurerm_service_plan" "main" {
   name                = "asp-${local.project_name}"
   resource_group_name = azurerm_resource_group.application.name
   location            = azurerm_resource_group.application.location
   os_type             = "Linux"
-  sku_name            = "P1v3" # local.sku_name
+  sku_name            = var.app_service_plan_sku
 
   tags = var.tags
 }
