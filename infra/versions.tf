d766dac4ca53000698c9ef9d4a99d17aca67a56c
Justin O'Connor <jsoconno@gmail.com>
2023-08-25 13:52:03 -0400

    add azuread to the required_providers block

--- infra/versions.tf
+++ infra/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.0"
